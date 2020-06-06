65ec5b2bec7020427ef04cacd6a16defe0f7ac2c
deryJ13 <dery.j13@gmail.com>
2020-06-05 23:25:19 -0400

    Complete Game Engine Logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -7,27 +7,69 @@ class Game extends Component {
     super()
 
     this.state = {
-      currentTurn: 1,
-      movesPlayed: 0,
-      board: ['', '', '', '', '', '', '', '', ''],
-      player1: 'x',
-      player2: 'o',
-      index: null,
-      value: null,
+      xIsNext: true,
       over: false,
       winner: ''
     }
   }
+  // variable/array to represent board
+  board = Array(9).fill(null)
 
-  mark = (event) => {
-    console.log(event.target.id)
-    console.log(this.state.player1)
-    document.getElementById(event.target.id).innerHTML = this.state.player1
+  // helper function to check for winner
+  checkWinner = () => {
+    const combos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+
+    for (let i = 0; i < combos.length; i++) {
+      const [a, b, c] = combos[i]
+      if (this.board[a] && this.board[a] === this.board[b] && this.board[a] === this.board[c]) {
+        return true
+      }
+    }
+  }
+
+  // helper function to check for draw
+  checkDraw = () => {
+    if (this.board.every((i) => i !== null)) {
+      return true
+    }
+  }
+
+  // helper function for marking and updating the board
+  handleClick = (event) => {
+    // we need to store the value of the cell to update api
+    const value = this.state.xIsNext ? 'x' : 'o'
+    // we need to re-assign and store the index to update api
+    const index = event.target.id
+    console.log('this is the index ' + index)
+    // conditional for illegal moves
+    if (this.board[event.target.id] !== null || this.state.over) {
+      return
+    }
+    // updating the board with new index/value combos
+    this.board[event.target.id] = value
+    // actually marking the board
+    document.getElementById(event.target.id).innerHTML = value
+    // conditional to prevent moves after a game is over
+    if (this.checkWinner() || this.checkDraw()) {
+      this.setState({ over: true })
+      return
+    }
+    // alternating turns
+    this.setState({ xIsNext: !this.state.xIsNext })
   }
 
   render () {
     return (
-      <GameBoard state={this.state} mark={this.mark}/>
+      <GameBoard state={this.state} onClick={this.handleClick}/>
     )
   }
 }
--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,18 +1,18 @@
 import React, { Fragment } from 'react'
 
-const GameBoard = ({ state, mark, x }) => (
+const GameBoard = ({ onClick }) => (
   <Fragment>
     <div className="container">
       <div id="game-board" className="game-container">
-        <div id="0" className="cell" onClick={mark}></div>
-        <div id="1" className="cell" onClick={mark}></div>
-        <div id="2" className="cell" onClick={mark}></div>
-        <div id="3" className="cell" onClick={mark}></div>
-        <div id="4" className="cell" onClick={mark}></div>
-        <div id="5" className="cell" onClick={mark}></div>
-        <div id="6" className="cell" onClick={mark}></div>
-        <div id="7" className="cell" onClick={mark}></div>
-        <div id="8" className="cell" onClick={mark}></div>
+        <div id="0" className="cell" onClick={onClick}></div>
+        <div id="1" className="cell" onClick={onClick}></div>
+        <div id="2" className="cell" onClick={onClick}></div>
+        <div id="3" className="cell" onClick={onClick}></div>
+        <div id="4" className="cell" onClick={onClick}></div>
+        <div id="5" className="cell" onClick={onClick}></div>
+        <div id="6" className="cell" onClick={onClick}></div>
+        <div id="7" className="cell" onClick={onClick}></div>
+        <div id="8" className="cell" onClick={onClick}></div>
       </div>
     </div>
   </Fragment>
